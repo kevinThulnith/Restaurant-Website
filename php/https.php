85857263e137b49d726c083d60d7617808d25cda
kevin <kevinthulnith@gmail.com>
2024-11-12 03:47:56 +0530

    twelth commit

--- php/https.php
+++ php/https.php
@@ -1,10 +1,10 @@
 <?php
+// Add security headers
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
+
 // Force HTTPS in PHP
 if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
   header('Location: https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
   exit;
 }
-
-// Add security headers
-header("X-Content-Type-Options: nosniff");
-header("X-Frame-Options: DENY");
